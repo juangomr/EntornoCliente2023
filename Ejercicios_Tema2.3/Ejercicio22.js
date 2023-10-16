539ae2bbf16a1096f7ab49fffb1ac1d2b3924bc9
CEP-AULA7-PC13\TECH <juangomr@gmail.com>
2023-10-16 20:59:24 +0200

    16/10

--- Ejercicios_Tema2.3/Ejercicio22.js
+++ Ejercicios_Tema2.3/Ejercicio22.js
@@ -8,13 +8,19 @@ function mostrar() {
   let fecha = new Date(formulario.fecha.value);
 
   a = fecha.getFullYear();
-  m = fecha.getMonth();
+  m = fecha.getMonth() + 1;
   d = fecha.getDate();
   if (a < 10) a = "0" + a;
-  if (m + 1 < 10) m = "0" + m;
+  if (m < 10) m = "0" + m;
   if (d < 10) d = "0" + d;
 
   window.opener.document.write(
-    `<h1>Datos introducidos:</h1> <br> ${nombre} <br> ${apellidos} <br> ${d}/${m}/${a}`
+    `<h1>Datos introducidos:</h1> <br>Su nombre es: ${nombre} <br>Su apellido es: ${apellidos} <br>Fecha de nacimiento: ${d}/${m}/${a} <br> Años bisiesto desde su fecha de nacimiento:`
   );
+
+  for (let index = Number(a); index < 2023; index++) {
+    if ((index % 4 == 0 && index % 100 != 0) || index % 400 == 0) {
+      window.opener.document.write(index + "<br>");
+    }
+  }
 }
